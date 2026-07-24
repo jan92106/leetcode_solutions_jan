39442957e7a927b524771c94285f6b99871dc69a
jan92106 <janviagrawal921@gmail.com>
2026-07-24 14:05:17 +0530

    Create 205-IsomorphicStrings

---
+++ 205-IsomorphicStrings/205-IsomorphicStrings.cpp
@@ -0,0 +1,16 @@
+// Last updated: 7/24/2026, 2:05:17 PM
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int mapS[256]={0};
+        int mapT[256]={0};
+        for(int i=0; i<s.length();i++){
+            if(mapS[s[i]] != mapT[t[i]]){
+                return false;
+            }
+            mapS[s[i]]=i+1;
+            mapT[t[i]]=i+1;
+        }
+        return true;
+    }
+};
